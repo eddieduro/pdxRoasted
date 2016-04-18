c7960935ef585b9781a663a206fd641effce7a7f
eddieduro <e.jamesduro@gmail.com>
2016-04-18 14:08:16 -0700

    Add search-results component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component, EventEmitter } from 'angular2/core';
-
+import { Component } from 'angular2/core';
+import { Roast } from './roast.model';
+import { SearchResultComponent } from './search-results.component';
 
 @Component({
   selector: 'my-app',
+  directives: [SearchResultComponent],
   template: `
-      <h1>Hello, Angular2!</h1>
+      <h1>PdxRoasted</h1>
+      <h3>Lorem</h3>
+      <search-results></search-results>
   `
 })
 export class AppComponent {
---
+++ app/search-results.component.ts
@@ -0,0 +1,14 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'search-results',
+  template: `
+          <select>
+            <option>1</option>
+            <option>2</option>
+          </select>
+  `
+})
+export class SearchResultComponent {
+
+}
